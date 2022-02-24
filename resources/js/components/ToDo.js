335b9aa2c3e16b9dcbb2a2a746a5c42df8b3d3fa
MasahiroKomoto <masahiro.komoto@oasys-tech.com>
2022-02-25 08:32:48 +0900

    ToDoを更新する

--- resources/js/components/ToDo.js
+++ resources/js/components/ToDo.js
@@ -2,11 +2,32 @@ import { Card, CardContent, CardHeader, TextField } from "@mui/material";
 import React from "react";
 import List from "@mui/material/List";
 import ToDoDetail from "./ToDoDetail";
+import { useUpdateToDoMutateTask } from "../hooks/ToDo";
 
 function ToDo(props) {
+  /** 更新用オブジェクト */
+  let toDo = {
+    id: props.toDo.id,
+    title: "",
+  };
+
+  /** 更新イベント */
+  const { updateToDoMutation } = useUpdateToDoMutateTask();
+  const useUpdateTodo = (event) => {
+    toDo.title = event.target.value;
+    updateToDoMutation.mutate(toDo);
+  };
+
+  /** テンプレート */
   return (
     <Card>
-      <CardHeader title={props.toDo.title} />
+      <TextField
+        variant="standard"
+        margin="dense"
+        defaultValue={props.toDo.title}
+        fullWidth
+        onChange={useUpdateTodo}
+      />
       <CardContent>
         <List>
           {props.toDo.to_do_details.map((detail) => {
