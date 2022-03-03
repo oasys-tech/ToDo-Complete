3e6a44c1e9efd3797d7f464687f21bd2f5a4d97b
MasahiroKomoto <masahiro.komoto@oasys-tech.com>
2022-03-04 08:26:38 +0900

    ToDo更新時にOptimistic Updateを適用する

--- resources/js/hooks/ToDo/useUpdateToDoMutateTask.js
+++ resources/js/hooks/ToDo/useUpdateToDoMutateTask.js
@@ -1,8 +1,39 @@
 import axios from "axios";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 
 const useUpdateToDoMutateTask = () => {
-  const updateToDoMutation = useMutation((toDo) => axios.put("/api/toDos/" + toDo.id, { title: toDo.title }));
+  const queryClient = useQueryClient();
+  const updateToDoMutation = useMutation(
+    (toDo) => axios.put("/api/toDos/" + toDo.id, { title: toDo.title }),
+    {
+      onMutate: async (toDo) => {
+        // 実行中の取得処理をキャンセル
+        await queryClient.cancelQueries("toDoList");
+
+        // 既存のToDoリストを取得する
+        const previousToDoList = queryClient.getQueriesData("toDoList");
+
+        // ToDoリストのキャッシュを更新する
+        queryClient.setQueryData("toDoList", (oldToDoList) =>
+          oldToDoList.map((oldToDo) => {
+            if (oldToDo.id == toDo.id) {
+              return {
+                ...oldToDo,
+                title: toDo.title,
+              };
+            }
+            return oldToDo;
+          })
+        );
+
+        // 更新に失敗した場合、既存のToDoリストを返却する
+        return { previousToDoList };
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries('toDoList');
+      },
+    }
+  );
   return { updateToDoMutation };
 };
 
